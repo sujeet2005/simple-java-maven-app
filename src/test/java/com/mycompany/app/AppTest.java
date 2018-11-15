15b97b4d7cc63a3d49f57bb4eef6a21921f390a6
Ketan Jobanputra <ketanvj@yahoo.com>
2018-11-15 15:43:41 +0530

    Moved sysout somewhere else.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -33,13 +33,13 @@ public class AppTest
     @Test
     public void testAppMain()
     {
-    	System.out.println("Inside testAppMain");
         App.main(null);
         try {
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
+    	System.out.println("Inside testAppMain");
     }
 
     @After
